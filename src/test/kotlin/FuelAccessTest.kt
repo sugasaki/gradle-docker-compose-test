04496a994e668bd4edbb0f84f18ba683858d6ed5
sugasaki <sugasaki@gmail.com>
2021-02-01 10:57:33 +0900

    port番号を変更 1080 -> 8080

--- src/test/kotlin/FuelAccessTest.kt
+++ src/test/kotlin/FuelAccessTest.kt
@@ -6,7 +6,7 @@ class FuelAccessTest {
 
     @Test
     fun `firstExampleExpectation test`() {
-        val url = "http://localhost:1080/firstExampleExpectation"
+        val url = "http://localhost:8080/firstExampleExpectation"
         val actual = FuelAccess.get(url)
         assertThat(actual.first).isEqualTo("some response1")
         assertThat(actual.second).isEqualTo(200)
@@ -14,7 +14,7 @@ class FuelAccessTest {
 
     @Test
     fun `secondExampleExpectation test`() {
-        val url = "http://localhost:1080/secondExampleExpectation"
+        val url = "http://localhost:8080/secondExampleExpectation"
         val actual = FuelAccess.get(url)
         assertThat(actual.first).isEqualTo("some response2")
         assertThat(actual.second).isEqualTo(200)
